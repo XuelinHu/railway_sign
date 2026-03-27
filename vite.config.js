b6b9e9c1cf45e37bc15191b19e6631105d09c904
XuelinHu <xuelin.hu@qq.com>
2026-03-27 11:17:42 +0800

    port

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,14 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue(), cesium()],
   server: {
-    port: 5173,
+    port: 4202,
     open: true,
   },
   preview: {
-    host: '0.0.0.0',
-    port: 4173,
+    host: "0.0.0.0",
+    port: 4201,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
-})
+});
